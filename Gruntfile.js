ec79fb22de3aa76a69c8f54ac8142b71600e5b22
Steven Bassett <steven.j.bassett@gmail.com>
2013-10-23 11:55:02 -0400

    Adding components

---
+++ Gruntfile.js
@@ -0,0 +1,166 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    watch: {
+        less: {
+          files: 'less/**/*',
+          tasks: ['less:development', 'autoprefixer:dist'],
+        },
+        js:{
+          files: 'js/**/*.js',
+          tasks: ['concat', 'jshint'],
+        },
+        images: {
+          files: 'images/**/*',
+          tasks: ['svgmin','imagemin']
+        },
+        livereload: {
+          files: 'dist/**/*',
+          options: {
+            livereload: true,
+          },
+        },
+    },
+    less: {
+      options: {
+          paths: ["less/**/*"],
+      },
+      development: {
+        files: {
+          "dist/css/style.css": "less/style.less",
+        },
+      },
+      production: {
+        files: {
+          "dist/css/style.min.css": "less/style.less",
+        },
+        options: {
+          yuicompress: true,
+          compress: true,
+          report: 'min',
+          strictImports: true
+        },
+      }
+    },
+
+    csslint: {
+      options: {
+        force: true,
+        formatters: [
+          {id: 'csslint-xml', dest: 'report/csslint.xml'}
+        ],
+      },
+      strict: {
+        options: {
+          import: 2
+        },
+        src: ['dist/css/**/*.css']
+      },
+      lax: {
+        options: {
+          import: false,
+          force: true,
+        },
+        src: ['dist/css/**/*.css']
+      }
+    },
+    jshint:{
+      files: ['js/**/*.js', 'Grutfile.js'],
+      options: {
+        force: true,
+        reporter: 'jslint',
+        reporterOutput: 'report/jshint.xml',
+        globals: {
+          jQuery: true,
+        },
+      },
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 8', 'ie 7']
+      },
+      dist: {
+        expand: true,
+        flatten: true,
+        src: 'dist/css/*.css',
+        dest: 'dist/css/'
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'images/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dist/images'                  // Destination path prefix
+        }],
+      }
+    },
+    svgmin: {                       // Task
+        options: {                  // Configuration that will be passed directly to SVGO
+          plugins: [{
+            removeViewBox: false
+          }]
+        },
+        dist: {                         // Target
+          files: [{                   // Dictionary of files
+            expand: true,           // Enable dynamic expansion.
+            cwd: 'images',         // Src matches are relative to this path.
+            src: ['**/*.svg'],      // Actual pattern(s) to match.
+            dest: 'dist/images',           // Destination path prefix.
+          }]
+        },
+      },
+    concat: {
+      js: {
+        files: {
+          'dist/js/dmc-squared.js': ['bower_components/bootstrap/dist/js/bootstrap.js','js/dmc-squared.js']
+        },
+      },
+    },
+    // copy:{
+    //   html:{
+    //     files: [
+    //       {src: 'static_htmls/*', dest: 'dist/'}
+    //     ]
+    //   },
+    //   js:{
+    //     files: [
+    //       {src: 'lib/selectivizr/selectivizr.js', dest: 'dist/js/selectivizr.js'},
+    //       {src:'js/vendor/modernizr/modernizr.js', dest: 'dist/js/modernizr.js'}
+    //     ]
+
+
+    //   }
+    // }
+
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+  // Default task(s).
+
+  grunt.registerTask('default', ['watch']);
+
+
+};
+
+
